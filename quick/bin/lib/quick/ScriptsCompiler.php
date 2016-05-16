4bf5393a3ebc52a92bba83706f1faac4f8d790e9
aoesky <360623358@qq.com>
2016-05-16 16:46:00 +0800

    bug fix

--- quick/bin/lib/quick/ScriptsCompiler.php
+++ quick/bin/lib/quick/ScriptsCompiler.php
@@ -309,7 +309,7 @@ class ScriptsCompiler
         // create ZIP archive
         $zipfile = $this->config['output'];
         $zip = new ZipArchive();
-        if (!$zip->open($zipfile, ZIPARCHIVE::OVERWRITE | ZIPARCHIVE::CM_STORE))
+        if (!$zip->open($zipfile,ZIPARCHIVE::CREATE | ZIPARCHIVE::OVERWRITE | ZIPARCHIVE::CM_STORE))
         {
             return false;
         }
